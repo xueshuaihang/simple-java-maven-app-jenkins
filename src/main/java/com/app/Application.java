a09d8cb706634c10d40e4b1bad5ecab0a4600a60
xueshuaihang <919395740@qq.com>
2020-06-30 16:03:38 +0800

    init project

---
+++ src/main/java/com/app/Application.java
@@ -0,0 +1,14 @@
+package com.app;
+
+/**
+ * @author Shuaihang Xue
+ * @date 2020/6/30
+ */
+public class Application {
+
+    public static String message = "Hello World";
+
+    public static void main(String[] args) {
+        System.out.println(message);
+    }
+}
